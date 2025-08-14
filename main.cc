a4021bd4ec84d88d53ae63017e1561e225a2ee63
kachornpat <kchornpg@gmail.com>
2025-08-14 21:01:44 +0700

    update light

--- main.cc
+++ main.cc
@@ -17,7 +17,7 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void inputProcess(GLFWwindow* window, float deltaTime);
 
-Camera ourCamera = NULL;
+Camera *ourCamera = NULL;
 
 float lastFrame = 0.0f;
 float lastX = 400, lastY = 800;
@@ -96,6 +96,52 @@ int main() {
 
     };
 
+    float vertices_basic[] = {
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+
+        -0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f, -0.5f,
+
+        -0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f
+
+    };
+
+
     unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
@@ -110,6 +156,16 @@ int main() {
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
 
+    unsigned int lightVAO, lightVBO;
+    glGenVertexArrays(1, &lightVAO);
+    glBindVertexArray(lightVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, lightVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_basic), vertices_basic, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+
     unsigned int textures[2];
     glGenTextures(2, textures);
 
@@ -117,7 +173,7 @@ int main() {
     glBindTexture(GL_TEXTURE_2D, textures[0]);
 
     int width, height, nrChannels;
-    unsigned char *data = stbi_load("C:/git/learn_opengl/container.jpg", &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
 
     if (data) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -136,7 +192,7 @@ int main() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     stbi_set_flip_vertically_on_load(true);
-    data = stbi_load("C:/git/learn_opengl/awesomeface.png", &width, &height, &nrChannels, 0);
+    data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
     if (data) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
@@ -146,33 +202,28 @@ int main() {
     }
     stbi_image_free(data);
 
-    Shader ourShader("C:/git/learn_opengl/shader/vertex.glsl", "C:/git/learn_opengl/shader/fragment.glsl");
+    Shader ourShader("shader/vertex.glsl", "shader/fragment.glsl"); 
 
     glm::mat4 model = glm::mat4(1.0f);
     unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
-    glm::vec3 cubePosition[] = {
-        glm::vec3( 0.0f,  0.0f,  0.0f),
-        glm::vec3( 2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3( 2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3( 1.3f, -2.0f, -2.5f),
-        glm::vec3( 1.5f,  2.0f, -2.50f),
-        glm::vec3( 1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
-    };
-    
-    glm::mat4 view;
-    unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
-    
-    glm::mat4 projection = glm::mat4(1.0f);
-    unsigned int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
+    // glm::vec3 cubePosition[] = {
+    //     glm::vec3( 0.0f,  0.0f,  0.0f),
+    //     glm::vec3( 2.0f,  5.0f, -15.0f),
+    //     glm::vec3(-1.5f, -2.2f, -2.5f),
+    //     glm::vec3(-3.8f, -2.0f, -12.3f),
+    //     glm::vec3( 2.4f, -0.4f, -3.5f),
+    //     glm::vec3(-1.7f,  3.0f, -7.5f),
+    //     glm::vec3( 1.3f, -2.0f, -2.5f),
+    //     glm::vec3( 1.5f,  2.0f, -2.50f),
+    //     glm::vec3( 1.5f,  0.2f, -1.5f),
+    //     glm::vec3(-1.3f,  1.0f, -1.5f)
+    // };
+
 
     int currentSec = 0;
     int countFrame = 0;
     
-    ourCamera = Camera(ourShader.ID);
+    ourCamera = new Camera(ourShader.ID);
 
     while (!glfwWindowShouldClose(window)) {
 
@@ -196,23 +247,28 @@ int main() {
         ourShader.setInt("texture1", 0);
         ourShader.setInt("texture2", 1);
 
-        ourCamera.use();
+        ourCamera->use();
         ourShader.use();
-        glBindVertexArray(VAO);
+        glBindVertexArray(lightVAO);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
 
-        for (unsigned int i = 0; i < 10; i++) {
-            model = glm::mat4(1.0f);
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // for (unsigned int i = 0; i < 10; i++) {
+        //     model = glm::mat4(1.0f);
 
 
-            // rotate * translate * vertexData
-            model = glm::translate(model, cubePosition[i]);
-            if (i % 3 == 0)
-                model = glm::rotate(model, (float) glfwGetTime(), glm::vec3(1.0f, 0.f, 0.5f));
+        //     // rotate * translate * vertexData
+        //     model = glm::translate(model, cubePosition[i]);
+        //     if (i % 3 == 0)
+        //         model = glm::rotate(model, (float) glfwGetTime(), glm::vec3(1.0f, 0.f, 0.5f));
             
-            float angle = 20.0f * i;
-            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
+        //     float angle = 20.0f * i;
+        //     glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        //     glDrawArrays(GL_TRIANGLES, 0, 36);
+        // }
         
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -234,13 +290,13 @@ void inputProcess(GLFWwindow *window, float deltaTime) {
 
     const float cameraSpeed = 1.5f * deltaTime;
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        ourCamera.Position += cameraSpeed * ourCamera.Front;
+        ourCamera->Position += cameraSpeed * ourCamera->Front;
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        ourCamera.Position -= cameraSpeed * ourCamera.Front;
+        ourCamera->Position -= cameraSpeed * ourCamera->Front;
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        ourCamera.Position -= glm::normalize(glm::cross(ourCamera.Front, ourCamera.Up)) * cameraSpeed;
+        ourCamera->Position -= glm::normalize(glm::cross(ourCamera->Front, ourCamera->Up)) * cameraSpeed;
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        ourCamera.Position += glm::normalize(glm::cross(ourCamera.Front, ourCamera.Up)) * cameraSpeed;
+        ourCamera->Position += glm::normalize(glm::cross(ourCamera->Front, ourCamera->Up)) * cameraSpeed;
 }
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height){
@@ -265,10 +321,10 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     xoffset *= sensitivity;
     yoffset *= sensitivity;
 
-    ourCamera.updateView(yoffset, xoffset);
+    ourCamera->updateView(yoffset, xoffset);
 
 }
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
-    ourCamera.updateFov((float) yoffset);
+    ourCamera->updateFov((float) yoffset);
 }
