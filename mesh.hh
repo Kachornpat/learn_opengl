1ded6f53dc921feeaa81792f6b3acef94e732f7b
kachornpat <kchornpg@gmail.com>
2025-09-28 21:04:44 +0700

    update: mesh class

---
+++ mesh.hh
@@ -0,0 +1,34 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include <vector>
+#include <string>
+#include <glm/glm.hpp>
+
+using namespace std;
+
+struct Vertex{
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture{
+    unsigned int id;
+    string type;
+};
+
+class Mesh{
+    public:
+        vector<Vertex> vertices;
+        vector<unsigned int> indices;
+        vector<Texture> textures;
+
+        Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures);
+    private:
+        unsigned int VAO, VBO, EBO;
+
+        void setupMesh();
+};
+
+#endif
