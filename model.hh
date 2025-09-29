4de419c031378a8adac03994ee8f3b4c6d90272e
kachornpat <kchornpg@gmail.com>
2025-09-29 16:01:08 +0700

    update: model class

---
+++ model.hh
@@ -0,0 +1,32 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <vector>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include "shader.hh"
+#include "mesh.hh"
+
+using namespace std;
+
+class Model
+{
+    public:
+        Model(char *path)
+        {
+            loadModel(path);
+        }
+        void Draw(Shader &shader);
+    private:
+
+        vector<Mesh> meshes;
+        string directory;
+        
+        void loadModel(string path);
+        void processNode(aiNode *node, const aiScene *scene);
+        Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+        vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, string typeName);
+};
+#endif
